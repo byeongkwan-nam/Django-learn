0732bb1281d5c91fa34fa4d762181e2b3f9dcf92
Byeongkwan <jangu3000@gmail.com>
2019-12-14 10:53:16 +0900

    edit: add user field avatar and gender

--- users/models.py
+++ users/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    bio = models.TextField(default='')
+    """ Custom User """
+
+    GENDER_MEN = "men"
+    GENDER_WOMEN = "women"
+    GENDER_OTHER = "other"
+    
+    GENDER_CHOICES = (
+        (GENDER_MEN, "Man"),
+        (GENDER_WOMEN, "Woman"),
+        (GENDER_OTHER, "Other")
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
+    bio = models.TextField(default='', blank=True)
 
 
