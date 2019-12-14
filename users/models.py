dba2e89a2963d252d673de00bf7bd187efad0819
Byeongkwan <jangu3000@gmail.com>
2019-12-14 11:06:07 +0900

    add: User - birth, currency, lang

--- users/models.py
+++ users/models.py
@@ -14,8 +14,28 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other")
     )
 
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW")
+    )
+
+    LANGUAGE_US = "us"
+    LANGUAGE_KR = "kr"
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_US, "US"),
+        (LANGUAGE_KR, "KR")
+    )
+
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(choices=GENDER_CHOICES, max_length=10, null=True, blank=True)
     bio = models.TextField(default='', blank=True)
+
+
+    birthdate = models.DateField(null=True)
+    currency = models.CharField(null=True, blank=True, max_length=3, choices=CURRENCY_CHOICES)
+    language = models.CharField(null=True, blank=True, max_length=2, choices=LANGUAGE_CHOICES)
+    superhost = models.BooleanField(default=False)
 
 
