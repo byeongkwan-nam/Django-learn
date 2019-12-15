ecbece488a2418b3ffad9dd6b6546752f1ca6095
Byeongkwan <jangu3000@gmail.com>
2019-12-15 15:41:08 +0900

    create: Rooms, Amenities, Facilities, Rules, Photos

--- rooms/models.py
+++ rooms/models.py
@@ -14,7 +14,31 @@ class AbstractItem(core_models.TimeStampedModel):
         return self.name
 
 class RoomType(AbstractItem):
-    pass
+    class Meta:
+        verbose_name = 'Room Type'
+        ordering = ['name']
+
+class Amenity(AbstractItem):
+    class Meta:
+        verbose_name_plural = 'Amenities'
+
+class HouseRule(AbstractItem):
+    class Meta:
+        verbose_name = 'House Rule'
+
+class Facility(AbstractItem):
+    class Meta:
+        verbose_name_plural = 'Facilities'
+
+class Photo(core_models.TimeStampedModel):
+    
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
+
 
 
 class Room(core_models.TimeStampedModel):
@@ -34,9 +58,13 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
 
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
 
-    room_type = models.ManyToManyField(RoomType, null=True)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
